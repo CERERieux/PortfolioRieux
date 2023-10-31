3984bc36546f8b536621310ad6f9e1fcf0b63c6f
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-10-31 15:28:53 -0600

    Types for anonboard exercise

--- src/client/types.d.ts
+++ src/client/types.d.ts
@@ -39,3 +39,33 @@ export interface TranslateResult {
   text: string;
   translation: string;
 }
+
+export interface BoardQuery {
+  id: string;
+  thread_count: number;
+}
+
+export interface ThreadQuery {
+  _id: string;
+  text: string;
+  created_on: string;
+  bumped_on: string;
+  replies: Reply[];
+  replycount: number;
+}
+
+export interface Reply {
+  _id: string;
+  text: string;
+  created_on: string;
+}
+
+export interface ErrorQuery {
+  error: string;
+}
+
+export interface CreateThread {
+  board: string;
+  text: string;
+  password: string;
+}
