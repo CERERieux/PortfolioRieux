a9544a64d21b28e99a8f287543d4fd8d191f8bce
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-04 16:32:47 -0600

    Exercise now are based on global user

--- src/server/schemas/basic.ts
+++ src/server/schemas/basic.ts
@@ -1,24 +1,18 @@
 import { Schema, model } from "mongoose";
-import type { IShortenerUrl, IUser, IExTracker } from "../types/basic";
+import type { IShortenerUrl, IExTracker } from "../types/basic";
 
 const shortUrlSchema = new Schema<IShortenerUrl>({
   shortUrl: { type: String, required: true },
   originalUrl: { type: String, required: true },
 });
 
-const userSchema = new Schema<IUser>({
-  username: { type: String, required: true },
-  count: { type: Number, default: 0 },
-  log: [{ type: Schema.Types.ObjectId, ref: "ExTracker" }],
-});
-
 const exTrackerSchema = new Schema<IExTracker>({
-  username: { type: Schema.Types.ObjectId, ref: "User" },
+  _id: { type: Schema.Types.ObjectId, required: true },
+  username: { type: String, ref: "GUser" },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: String,
 });
 
 export const Url = model<IShortenerUrl>("Url", shortUrlSchema);
-export const User = model<IUser>("User", userSchema);
 export const ExTracker = model<IExTracker>("ExTracker", exTrackerSchema);
