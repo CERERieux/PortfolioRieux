5b7707463ed6145bc23c15c7774aa848f4553791
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-04 16:36:40 -0600

    Types of global user and extension of Express type

---
+++ src/server/types/global.d.ts
@@ -0,0 +1,29 @@
+import type { IExTracker, IShortenerUrl } from "./basic";
+import type { IBook, IIssueTracker } from "./advanced";
+
+declare global {
+  namespace Express {
+    interface Request {
+      _id: string;
+    }
+  }
+}
+
+export interface IGlobalUser {
+  _id: string;
+  password: string;
+  img: string;
+  exercises: IExTracker[];
+  issues: IIssueTracker[];
+  books: IBook[];
+  shortUrl: IShortenerUrl[];
+}
+
+export interface ReqBodyCreateUser {
+  _id: string;
+  password: string;
+}
+
+export interface TokenGUser {
+  username: string;
+}
