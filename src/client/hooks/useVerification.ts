72b8603e10046f04037cb6fa6f505a7f9bc37cba
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-14 20:42:42 -0600

    Hook to verificate if token is valid for service

---
+++ src/client/hooks/useVerification.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useUser } from "../store/user";
+import { ERROR_GUSER } from "../../server/schemas/global";
+import { type ErrorAuth } from "../types";
+
+/** Custom hook that validates our token to access to our services from
+ * the user interface, it returns a boolean that indicates if the validation
+ * was successful or another variable that contains the error and the cause
+ * of it
+ * And the token of the user also is added here
+ */
+export function useVerification() {
+  const { token, verifyToken } = useUser();
+  const [errorAuth, setErrorAuth] = useState<ErrorAuth>({
+    message: "",
+    cause: null,
+  });
+  const [enableEx, setEnableEx] = useState(false);
+
+  useEffect(() => {
+    const verification = async () => {
+      const resultToken = await verifyToken(token);
+      if (resultToken === ERROR_GUSER.EXPIRED_TOKEN) {
+        setErrorAuth({ message: resultToken, cause: "ExpiredToken" });
+      } else if (resultToken === ERROR_GUSER.ERROR_VERIFY_TOKEN) {
+        setErrorAuth({ message: resultToken, cause: "BadToken" });
+      } else {
+        setEnableEx(true);
+      }
+    };
+
+    verification();
+  }, []);
+
+  return {
+    token,
+    validFetch: enableEx,
+    errorAuth,
+  };
+}
