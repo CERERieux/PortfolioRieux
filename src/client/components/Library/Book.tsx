759b2b78cc10f987177f495f89300e5629baf8f0
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-16 20:23:31 -0600

    Progress on interfaces for book service

--- src/client/components/Library/Book.tsx
+++ src/client/components/Library/Book.tsx
@@ -1,34 +1,205 @@
-import { useEffect } from "react"
+import { type ChangeEvent, useEffect, useState, type FormEvent } from "react"
 import { isAxiosError } from "axios"
 import { useSingleBook } from "../../hooks/useSingleBook"
+import { Link, useParams } from "react-router-dom"
+import type { SingleBook, BookStatus } from "../../types"
+
+function isEmptyNotes(data: SingleBook) {
+    const isEmpty = data.notes.map(note => {
+        if (note !== "") return true
+        return false
+    })
+    console.log(isEmpty)
+    if (isEmpty.find(note => note) !== undefined) {
+        return false
+    } else
+        return true
+}
 
 export default function Book() {
-    const { data, errorAuth, errorBook } = useSingleBook("654ab7a893cee4af8a535d54")
+    const { id } = useParams()
+    const idBook = id ?? ""
+    const { data, errorAuth, errorBook, updateBook, addNote, removeNote, token } = useSingleBook(idBook)
+    const [title, setTitle] = useState("")
+    const [review, setReview] = useState("")
+    const [status, setStatus] = useState<BookStatus>("Plan to Read")
+    const [recommend, setRecommend] = useState<boolean | undefined>(undefined)
+    const [localError, setLocalError] = useState<string | null>(null)
+    const [note, setNote] = useState("")
+    const [emptyNotes, setEmptyNotes] = useState(true)
 
     useEffect(() => {
-        console.log(data)
+        if (data !== undefined) {
+            setTitle(data.title)
+            setReview(data.review)
+            setStatus(data.status as BookStatus)
+            setRecommend(data.recommend)
+            setEmptyNotes(isEmptyNotes(data))
+        }
     }, [data])
+
+    useEffect(() => {
+        if (updateBook.isSuccess) {
+            setLocalError("")
+        }
+        else if (updateBook.isError) {
+            const { error } = updateBook
+            if (isAxiosError(error)) {
+                setLocalError(error.response?.data.error)
+            }
+            else {
+                setLocalError("Something went wrong at emptying your Library")
+            }
+        }
+    }, [updateBook.isSuccess])
+
+    useEffect(() => {
+        if (addNote.isSuccess) {
+            if (data !== undefined) {
+                setEmptyNotes(isEmptyNotes(data))
+            }
+            setNote("")
+            setLocalError("")
+        }
+        else if (addNote.isError) {
+            const { error } = addNote
+            if (isAxiosError(error)) {
+                setLocalError(error.response?.data.error)
+            }
+            else {
+                setLocalError("Something went wrong at emptying your Library")
+            }
+        }
+    }, [addNote.isSuccess])
+
+    useEffect(() => {
+        if (removeNote.isSuccess) {
+            if (data !== undefined) {
+                setEmptyNotes(isEmptyNotes(data))
+            }
+            setLocalError("")
+        }
+        else if (removeNote.isError) {
+            const { error } = removeNote
+            if (isAxiosError(error)) {
+                setLocalError(error.response?.data.error)
+            }
+            else {
+                setLocalError("Something went wrong at emptying your Library")
+            }
+        }
+    }, [removeNote.isSuccess])
+
+    const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+    const handleStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+        setStatus(e.target.value as BookStatus)
+    }
+    const handleRecommend = (e: ChangeEvent<HTMLSelectElement>) => {
+        let recom: (boolean | undefined)
+        if (e.target.value === "Yes") recom = true
+        if (e.target.value === "No") recom = false
+        setRecommend(recom)
+    }
+    const handleReview = (e: ChangeEvent<HTMLInputElement>) => {
+        setReview(e.target.value)
+    }
+
+    const handleUpdate = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        let recommendString = "I can't say"
+        if (recommend === true) recommendString = "Yes"
+        if (recommend === false) recommendString = "No"
+        if (title !== "") {
+            updateBook.mutate({ id: idBook, token, title, review, status, recommend: recommendString })
+        }
+        else {
+            setLocalError("Please don't leave the title field empty")
+        }
+
+    }
+
+
+    const handleNote = (e: ChangeEvent<HTMLInputElement>) => {
+        setNote(e.target.value)
+    }
+    const handleNoteSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (note !== "")
+            addNote.mutate({ id: idBook, token, note })
+        else {
+            setLocalError("Please don't leave the note field empty")
+        }
+    }
+    const handleDeleteNote = (number: string) => {
+        removeNote.mutate({ id: idBook, token, number })
+    }
+
     return (
         <div>
             <div>
                 {errorAuth.cause !== null && <h1>Error: You are not logged in to use this service </h1>}
                 {errorBook !== null && isAxiosError(errorBook) && <h1>Error: {errorBook.response?.data.error}</h1>}
+                {localError !== null && <h2>{localError}</h2>}
             </div>
+            <Link to="/services/library"><button>Return to Library</button></Link>
+            <h2>Update your book!</h2>
+            <form onSubmit={handleUpdate}>
+                <label htmlFor="">
+                    Title: <input type="text" value={title} onChange={handleTitle} />
+                </label>
+                <label htmlFor="">
+                    Review: <input type="text" value={review} onChange={handleReview} />
+                </label>
+                <label htmlFor="">
+                    Status: <select name="" id="" value={status} onChange={handleStatus}>
+                        <option value="Plan to Read">Plan to Read</option>
+                        <option value="Current Reading">Current Reading</option>
+                        <option value="Completed">Completed</option>
+                        <option value="Dropped/Unfinish">Dropped/Unfinish</option>
+                    </select>
+                </label>
+                <label htmlFor="">
+                    Do you recommend it? <select
+                        value={recommend === undefined ? "I can't say" : recommend ? "Yes" : "No"}
+                        onChange={handleRecommend}>
+                        <option value="I can't say">I can&apos;t say</option>
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                    </select>
+                </label>
+                <button>Update book!</button>
+            </form>
+            <h2>Add a note to your book</h2>
+
+            <form onSubmit={handleNoteSubmit}>
+                <label htmlFor="">
+                    Note: <input type="text" value={note} onChange={handleNote} />
+                </label>
+                <button>Add Note!</button>
+            </form>
             <div>
                 {data !== undefined ?
-                    <ul>
-                        {
-                            <li key={data._id.toString()}>
-                                <div>
-                                    <h2>{data.title}</h2>
-                                    <p>{data.status}</p>
-                                    <p>{data.review}</p>
-                                    <p>{data.recommend}</p>
-                                    <p>{data.notes}</p>
-                                </div>
-                            </li>
-                        }
-                    </ul>
+                    <div>
+                        <h2>Title: {data.title}</h2>
+                        <p>Status: {data.status}</p>
+                        <p>Review: {data.review !== "" ? data.review : "You haven't review this book."}</p>
+                        <p>Do you recommend it? {data.recommend === undefined ? "I can't say" : data.recommend ? "Yes" : "No"}</p>
+                        <p>Notes: {emptyNotes && "Here will appear the notes you add to your book!"}</p>
+                        <ul>
+                            {data.notes.map((note, i) => {
+                                if (note !== "")
+                                    return (
+                                        <li key={i}>
+                                            {note}
+                                            <button onClick={() => { handleDeleteNote(`${i}`) }}>Remove note</button>
+                                        </li>
+                                    )
+                                return null
+                            })}
+                        </ul>
+                    </div>
                     : <p>Loading...</p>}
             </div>
         </div>
--- src/client/components/Library/Library.tsx
+++ src/client/components/Library/Library.tsx
@@ -1,36 +1,156 @@
-import { useEffect } from "react"
+import { type ChangeEvent, useEffect, useState, type FormEvent } from "react"
 import { useBooks } from "../../hooks/useBooks"
 import { isAxiosError } from "axios"
+import { type BookStatus } from "../../types"
+import { ERROR_BOOKS } from "../../../server/schemas/advanced"
+import { useNavigate, Link } from "react-router-dom"
 
 export default function Library() {
-    const { data, errorAuth, errorBook } = useBooks()
+    const { data, errorAuth, errorBook, createNewBook, deleteLibrary, removeBook, token } = useBooks()
+    const [title, setTitle] = useState("")
+    const [status, setStatus] = useState<BookStatus>("Plan to Read")
+    const [errorLocal, setErrorLocal] = useState<string | null>(null)
+    const [action, setAction] = useState<string | null>(null)
+    const navigate = useNavigate()
+
 
     useEffect(() => {
-        console.log(data)
-    }, [data])
+        // If the creation of a book was a success, put the form inputs to its default state
+        if (createNewBook.isSuccess) {
+            const bookID = createNewBook.data._id.toString()
+            setTitle("")
+            setStatus("Plan to Read")
+            setErrorLocal(null)
+            setAction("New book added! Redirecting you to your new book added...")// Later change this to redirect to new book
+            setTimeout(() => { setAction(null) }, 2000)
+            setTimeout(() => { navigate(`/services/library/${bookID}`) }, 2000)
+        }
+        else if (createNewBook.isError) {
+            // If it wasn't a success, maybe was an error, if that is the case
+            // Show an error about it
+            const { error } = createNewBook
+            if (isAxiosError(error)) {
+                setErrorLocal(error.response?.data.error)
+            }
+            else {
+                setErrorLocal("Something went wrong at creating your book")
+            }
+        }
+    }, [createNewBook.isSuccess])
+
+    useEffect(() => {
+        // If library was emptied successfully, show the action
+        if (deleteLibrary.isSuccess) {
+            setAction("You emptied all your Library!")
+            setTimeout(() => { setAction(null) }, 5000)
+        }
+        else if (deleteLibrary.isError) {
+            /* If it wasn't a success, then maybe is an error,
+            Show it in case it is, ex: User maybe wants to delete
+            An empty library and that can't be done */
+            const { error } = deleteLibrary
+            if (isAxiosError(error)) {
+                setErrorLocal(error.response?.data.error)
+            }
+            else {
+                setErrorLocal("Something went wrong at emptying your Library")
+            }
+        }
+    }, [deleteLibrary.isSuccess])
+
+    useEffect(() => {
+        // If book was removed successfully, show the action
+        if (removeBook.isSuccess) {
+            setAction("Book removed!")
+            setTimeout(() => { setAction(null) }, 2000)
+        }
+        else if (removeBook.isError) {
+            /* If it wasn't a success, then maybe is an error,
+            Show it in case it was one */
+            const { error } = deleteLibrary
+            if (isAxiosError(error)) {
+                setErrorLocal(error.response?.data.error)
+            }
+            else {
+                setErrorLocal("Something went wrong at emptying your Library")
+            }
+        }
+    }, [removeBook.isSuccess])
+
+    const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+    const handleStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+        setStatus(e.target.value as BookStatus)
+    }
+    const handleBookSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (title !== "") {
+            createNewBook.mutate({ title, status, token })
+        }
+        else {
+            setErrorLocal("Please introduce a title if you want to add a book")
+        }
+
+    }
+    const handleDeleteLibrary = () => {
+        if (data !== undefined && !("error" in data))
+            deleteLibrary.mutate({ token })
+        else {
+            setErrorLocal(ERROR_BOOKS.DELETE_EMPTY_LIBRARY)
+        }
+    }
+    const handleRemove = (id: string) => {
+        removeBook.mutate({ id, token })
+    }
+
     return (
         <div>
             <div>
                 {errorAuth.cause !== null && <h1>Error: You are not logged in to use this service </h1>}
                 {errorBook !== null && isAxiosError(errorBook) && <h1>Error: {errorBook.response?.data.error}</h1>}
+                {errorLocal !== null && <h2>Error: {errorLocal}</h2>}
+                {action !== null && <h2>{action}</h2>}
             </div>
-            <div>
-                {data !== undefined ?
-                    <ul>
-                        {data.map(book => {
-                            const id = book._id.toString()
-                            return <li key={id}>
-                                <div>
-                                    <h2>{book.title}</h2>
-                                    <p>{book.status}</p>
-                                    <p>{book.review}</p>
-                                    <p>{book.recommend}</p>
-                                </div>
-                            </li>
-                        })}
-                    </ul>
-                    : <p>Loading...</p>}
-            </div>
+            <h2>Add books to your library!</h2>
+            <form onSubmit={handleBookSubmit}>
+                <label htmlFor="">
+                    Title: <input type="text" name="title" value={title} onChange={handleTitle} />
+                </label>
+                <label htmlFor="">
+                    Status: <select name="" id="" value={status} onChange={handleStatus}>
+                        <option value="Plan to Read">Plan to Read</option>
+                        <option value="Current Reading">Current Reading</option>
+                        <option value="Completed">Completed</option>
+                        <option value="Dropped/Unfinish">Dropped/Unfinish</option>
+                    </select>
+                </label>
+                <button disabled={createNewBook.isPending}>Put it in the Library!</button>
+            </form>
+            <button onClick={handleDeleteLibrary} disabled={data !== undefined && ("error" in data)}>Delete all your books!!!</button>
+            {data !== undefined ?
+                <div>
+                    {!("error" in data) ?
+                        <ul>
+                            {data.map(book => {
+                                const id = book._id.toString()
+                                return <li key={id}>
+                                    <div>
+                                        <Link to={`/services/library/${id}`}><h2>{book.title}</h2></Link>
+                                        <p>{book.status}</p>
+                                        <p>{book.review}</p>
+                                        <p>{book.recommend}</p>
+                                        <button onClick={() => { handleRemove(id) }}>Remove from my Library</button>
+                                    </div>
+                                </li>
+                            })}
+                        </ul>
+                        : <h2>{data.error}</h2>
+                    }
+
+                </div>
+                : <p>Loading...</p>}
+
         </div>
     )
 }
