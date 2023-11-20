d8e60dd3b60a3af8ad0fd95bc53b700e553746f6
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-20 15:20:10 -0600

    Need to think how to do it, this is a sketch

---
+++ src/client/hooks/useAdmin.ts
@@ -0,0 +1,19 @@
+// import { useEffect, useState } from "react";
+// import { useUser } from "../store/user";
+// import { ERROR_GUSER } from "../../server/schemas/global";
+// import type { ErrorAuth } from "../types";
+// import { useVerification } from "./useVerification";
+// import { useQuery } from "@tanstack/react-query";
+
+// export function useAdmin() {
+//   const {errorAuth,token,validFetch} = useVerification()
+//   const [userProfile, setUserProfile] = useState<null|string>(null)
+//   const getUsers = useQuery({
+//     queryKey: ["users"],
+//     queryFn:
+//   })
+//   /**TODO piensa que quieres, si tener un admin que ve las cosas a traves de usuarios o
+//    * lo ve todo y lo filtra como si fuera un ususario, para ver las issues, url y esas
+//    * cosas para "moderar" el portafolio o las 2
+//    */
+// }
