58a1a4203f1edb603cf2569f499c8bd9e07869f7
CERERieux <59849778+CERERieux@users.noreply.github.com>
2023-11-27 15:10:45 -0600

    Fixed logic when user have expired token

--- src/client/components/Login/Login.tsx
+++ src/client/components/Login/Login.tsx
@@ -3,19 +3,31 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import LogInForm from "./LoginForm"
 import SignInForm from "./SignInForm"
+import { ERROR_GUSER } from "../../../server/schemas/global"
 
 export default function Login() {
     const navigate = useNavigate()
-    const { action, error, token, createUser, loginUser } = useUser()
+    const { action, error, token, createUser, loginUser, logoffUser, verifyToken } = useUser()
     const [signIn, setSignIn] = useState(false)
 
     // An useEffect hook to redirect user in case they is already logged in
     useEffect(() => {
-        if (token !== "") {
-            setTimeout(() => {
-                navigate("/home")
-            }, 1000)
+        const verifyStart = async () => {
+            let result
+            if (token !== "")
+                result = await verifyToken(token)
+            // In case user token is expired or is bad we need to login again
+            if (result === ERROR_GUSER.EXPIRED_TOKEN || result === ERROR_GUSER.ERROR_VERIFY_TOKEN) {
+                logoffUser()    // Logout user to remove token
+            }
+            else if (token !== "") {
+                // If user is logged in and it's valid, redirect to home
+                setTimeout(() => {
+                    navigate("/home")
+                }, 1000)
+            }
         }
+        verifyStart()
     }, [])
 
     const handleView = () => {
