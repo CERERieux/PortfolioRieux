c2128aef9e7a5bdc0d2381d150cfaeffaf349d00
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-01-22 19:13:21 -0600

    Add Tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/client/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        logCalculator: ['Oxanium', ...defaultTheme.fontFamily.sans],
+        digitalDisplay: ['"Chakra Petch"', ...defaultTheme.fontFamily.serif],
+        buttonCalculator: ['Sriracha', ...defaultTheme.fontFamily.mono],
+      }
+    },
+  },
+  plugins: [],
+}
+
