67f0736f75212d66f441c1d7633135418b8b62b7
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-01-30 15:39:50 -0600

    Move fetch to axios for converter

---
+++ src/client/services/converter.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import type { ConverterResult } from "../types";
+
+export function convertUnit(dataConvert: string) {
+  return axios<ConverterResult>({
+    url: `/cYSvQmg9kR/advanced/converter?input=${dataConvert}`,
+    method: "get",
+  })
+    .then(({ data }) => data)
+    .catch(err => {
+      console.error(err);
+      return { error: err.response.data.error as string };
+    });
+}
