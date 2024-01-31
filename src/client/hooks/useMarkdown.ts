5b062d3093febc0a1ccd7de7ce23a8c70acb0f95
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-01-31 14:49:50 -0600

    Add function for visibility of preview

--- src/client/hooks/useMarkdown.ts
+++ src/client/hooks/useMarkdown.ts
@@ -3,13 +3,16 @@ import { useState } from "react"; // Function to save our App state
 import { sanitizeInput } from "../utils/sanitize";
 
 // An example of markdown that displays at the start
-const PREVIEW_TEXT = `# Header size H1
-## Header size H2
-A link that send you to a [Youtube page](https://www.youtube.com/watch?v=r4XqG6nnvkY).
-We put a bit of code, look: \`let x = 9\`.
+const PREVIEW_TEXT = `# Markdown Parser
+## A preview of what you can do with Markdown!
+
+You can put a link that send you to a Youtube video [OMORI - Forest Frenzy](https://www.youtube.com/watch?v=r4XqG6nnvkY).
+(**Note:** This link will not open a new window...)  
+
+You can put a bit of code, look: \`let x = 9\`.
 
 \`\`\`
-// this is multi-line code:
+// You can also do a multi-line code:
 function anotherExample(firstLine, lastLine) {
     if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
         return multiLineCode;
@@ -17,15 +20,24 @@ function anotherExample(firstLine, lastLine) {
 }
 \`\`\`
 
-A list item:
-- 1 item
-- 2 items
+An unordered list item:
+- Flowers
+- Sugar
+  - Brown
+  - Refined
+  - Colored
+- Eggs
+
+An ordered list item:
+1. Please break the egg and separate the yolk.
+2. Mix with flour until you have a solid dough.
 
 A blockquote:
->Look, I'm  inside a **blockquote**
+>A person who never made a mistake never tried anything new 
+*-Albert Einstein*
 
 An image of mountains:
-![The Everest](https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1024px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg)`;
+![The Everest](https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/1024px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg)*A stunning view of a mountain.*`;
 
 // Options for the parser, it makes carriage returns appear as <br/>
 marked.use({
@@ -36,10 +48,17 @@ marked.use({
 // Our custom hook that makes the work of manage the user input and translate it
 export function useMarkdown() {
   const [markdown, setMarkdown] = useState(PREVIEW_TEXT); // Our initial state is the example markdown
+  const [size, setSize] = useState<"min" | "max">("min");
   const handleEditorChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMarkdown(e.target.value); // Each time the user changes the textarea in the app, we save the change
   };
-
+  const deleteContent = () => {
+    setMarkdown("");
+  };
+  const changeCurrentSize = () => {
+    if (size === "min") setSize("max");
+    else setSize("min");
+  };
   // Function that accepts a string and parse it to HTML Elements
   function translateMarkdown(input: string) {
     // But before parsing, sanitize the input
@@ -48,5 +67,12 @@ export function useMarkdown() {
   }
 
   // Return the markdown, the parser and the function that change the markdown when textarea changes
-  return { markdown, handleEditorChange, translateMarkdown };
+  return {
+    markdown,
+    handleEditorChange,
+    translateMarkdown,
+    deleteContent,
+    size,
+    changeCurrentSize,
+  };
 }
