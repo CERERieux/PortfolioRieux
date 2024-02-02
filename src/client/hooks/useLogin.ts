0cb81d4a9cce9a69c476295ae15b2b63ac919b91
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-02 16:36:47 -0600

    Custom hook for login interface

---
+++ src/client/hooks/useLogin.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useUser } from "../store/user";
+import { ERROR_GUSER } from "../../server/schemas/global";
+import { useNavigate } from "react-router-dom";
+
+export default function useLogin() {
+  const navigate = useNavigate();
+  const { token, createUser, loginUser, logoffUser, verifyToken } = useUser();
+  const [signIn, setSignIn] = useState(false);
+
+  // An useEffect hook to redirect user in case they is already logged in
+  useEffect(() => {
+    const verifyStart = async () => {
+      let result;
+      if (token !== "") result = await verifyToken(token);
+      // In case user token is expired or is bad we need to login again
+      if (
+        result === ERROR_GUSER.EXPIRED_TOKEN ||
+        result === ERROR_GUSER.ERROR_VERIFY_TOKEN
+      ) {
+        logoffUser(); // Logout user to remove token
+      } else if (token !== "") {
+        // If user is logged in and it's valid, redirect to home
+        setTimeout(() => {
+          navigate("/home");
+        }, 1000);
+      }
+    };
+    verifyStart();
+  }, []);
+
+  const handleView = () => {
+    setSignIn(state => !state);
+    console.log(signIn);
+  };
+
+  return {
+    handleView,
+    view: signIn,
+    setView: setSignIn,
+    createUser,
+    loginUser,
+  };
+}
