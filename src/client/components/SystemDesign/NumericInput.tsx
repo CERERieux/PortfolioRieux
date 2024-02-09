3ff7914654d4b61ba53121d8188e43a73bac34c0
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-09 12:01:12 -0600

    Add new component to DS

---
+++ src/client/components/SystemDesign/NumericInput.tsx
@@ -0,0 +1,43 @@
+import { type ChangeEvent } from "react";
+
+interface NumericInputProps {
+  min?: number;
+  max?: number;
+  stepNum?: number;
+  disabled?: boolean;
+  placeholder?: string;
+  styles: string;
+  id: string;
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function NumericInput({
+  min,
+  max,
+  styles = "h-6 w-16 text-sm shadow-md shadow-black/50",
+  disabled = false,
+  placeholder,
+  stepNum,
+  id,
+  value,
+  onChange,
+}: NumericInputProps) {
+  return (
+    <div className="relative">
+      <input
+        type="number"
+        min={min}
+        max={max}
+        className={`${styles} rounded-2xl p-0 px-2 py-1 invalid:border-red-500 invalid:focus:ring-red-500`}
+        disabled={disabled}
+        placeholder={placeholder}
+        step={stepNum}
+        required
+        id={id}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
