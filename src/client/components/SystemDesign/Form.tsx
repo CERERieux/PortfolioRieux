c031ca9e968c923365b423ebac9ae7c1638a498a
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-12 15:05:46 -0600

    Change name to Form

--- src/client/components/SystemDesign/Form.tsx
+++ src/client/components/SystemDesign/Form.tsx
@@ -3,13 +3,15 @@ import { type FormEvent } from "react";
 interface LoginFormProps {
   children: React.ReactNode;
   onSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  id?: string;
 }
 
-export default function LoginForm({ children, onSubmit }: LoginFormProps) {
+export default function LoginForm({ children, onSubmit, id }: LoginFormProps) {
   return (
     <form
       onSubmit={onSubmit}
-      className="flex h-full w-full flex-col items-center justify-around [&_span]:w-1/4 [&_span]:text-right [&_span]:md:w-1/5"
+      className="flex h-full w-full flex-col items-center justify-around gap-4 [&_span]:w-1/4 [&_span]:text-right [&_span]:md:w-1/5"
+      id={id}
     >
       {children}
     </form>
