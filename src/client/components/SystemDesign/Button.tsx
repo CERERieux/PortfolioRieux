73c9787093220168433420059cd6c9018322e0ab
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-12 19:52:09 -0600

    Add disabled prop

--- src/client/components/SystemDesign/Button.tsx
+++ src/client/components/SystemDesign/Button.tsx
@@ -4,6 +4,7 @@ interface PropsButton {
   mediaSize?: string;
   children: React.ReactNode;
   onClick?: () => void;
+  disabled?: boolean;
 }
 export default function Button({
   color,
@@ -11,12 +12,14 @@ export default function Button({
   mediaSize,
   children,
   onClick,
+  disabled,
 }: PropsButton) {
   const size = xSize ?? "w-1/2";
   return (
     <button
       className={`rounded-xl border px-2 py-1 hover:border-black hover:text-white ${color} ${size} ${mediaSize}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
