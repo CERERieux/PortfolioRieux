7a94e1c754088583fde914ba34867101c1edfab4
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-13 20:00:04 -0600

    Add required field to add '*' after its children

--- src/client/components/SystemDesign/TitleInput.tsx
+++ src/client/components/SystemDesign/TitleInput.tsx
@@ -1,10 +1,20 @@
 interface TitleInputProps {
   children: React.ReactNode;
-  firstColor: string;
+  firstColor?: string;
+  required?: boolean;
 }
 
-export default function TitleInput({ children, firstColor }: TitleInputProps) {
+export default function TitleInput({
+  children,
+  firstColor,
+  required = false,
+}: TitleInputProps) {
+  const styleRequired = required
+    ? "after:content-['*'] after:text-red-500"
+    : "";
   return (
-    <span className={`first-letter:text-lg ${firstColor}`}>{children}</span>
+    <span className={`first-letter:text-lg ${firstColor} ${styleRequired}`}>
+      {children}
+    </span>
   );
 }
