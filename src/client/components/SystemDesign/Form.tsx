ac74f9ee834e96d5e65bdbf3f07d6e37d77215d6
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-13 20:01:39 -0600

    Add fields to make it more reusable

--- src/client/components/SystemDesign/Form.tsx
+++ src/client/components/SystemDesign/Form.tsx
@@ -2,15 +2,26 @@ import { type FormEvent } from "react";
 
 interface LoginFormProps {
   children: React.ReactNode;
-  onSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  submitAsync?: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+  submitFn?: (e: FormEvent<HTMLFormElement>) => void;
   id?: string;
+  style?: string;
+  mdMedia?: string;
 }
 
-export default function LoginForm({ children, onSubmit, id }: LoginFormProps) {
+export default function Form({
+  children,
+  submitAsync,
+  submitFn,
+  id,
+  style = "items-center justify-around gap-4",
+  mdMedia = "[&_span]:md:w-1/5",
+}: LoginFormProps) {
+  const onSubmit = submitAsync ?? submitFn;
   return (
     <form
       onSubmit={onSubmit}
-      className="flex h-full w-full flex-col items-center justify-around gap-4 [&_span]:w-1/4 [&_span]:text-right [&_span]:md:w-1/5"
+      className={`flex h-full w-full flex-col ${style} [&_span]:w-1/4 [&_span]:text-right ${mdMedia}`}
       id={id}
     >
       {children}
