08097a8939ed73cd3256e5c0562d085ecae50038
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-15 19:27:44 -0600

    Move position of action button

--- src/client/components/ExTracker/DeleteButton.tsx
+++ src/client/components/ExTracker/DeleteButton.tsx
@@ -22,7 +22,7 @@ export default function DeleteButton({
       coverColor="bg-slate-200 shadow-slate-100"
       hoverColor="hover:bg-red-100 hover:shadow-red-400/30 hover:text-red-500"
       groupName={["group/delete", "group-hover/delete:block"]}
-      position="top-0 right-8"
+      position="right-0 bottom-2"
       tooltipText="Delete"
       tooltipPos="-bottom-5 -left-0"
     >
