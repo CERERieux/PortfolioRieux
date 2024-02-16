3fe513d210e2a991fa9017a812e5172767d43356
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-15 19:24:17 -0600

    Add way to put extra styles

--- src/client/components/SystemDesign/ActionMessage.tsx
+++ src/client/components/SystemDesign/ActionMessage.tsx
@@ -1,10 +1,16 @@
 interface ActionMessageProps {
   children: React.ReactNode;
+  extraStyles?: string;
 }
 
-export default function ActionMessage({ children }: ActionMessageProps) {
+export default function ActionMessage({
+  children,
+  extraStyles,
+}: ActionMessageProps) {
   return (
-    <div className="absolute top-10 mx-auto w-full text-pretty rounded-full bg-green-200 px-2 py-1 text-center text-sm text-green-700 md:w-1/2 md:text-base ">
+    <div
+      className={`absolute top-10 mx-auto w-full text-pretty rounded-full bg-green-200 px-2 py-1 text-center text-sm text-green-700 md:w-1/2 md:text-base ${extraStyles}`}
+    >
       {children}
     </div>
   );
--- src/client/components/SystemDesign/ErrorMessage.tsx
+++ src/client/components/SystemDesign/ErrorMessage.tsx
@@ -1,10 +1,16 @@
 interface ErrorMessageProps {
   children: React.ReactNode;
+  extraStyles?: string;
 }
 
-export default function ErrorMessage({ children }: ErrorMessageProps) {
+export default function ErrorMessage({
+  children,
+  extraStyles,
+}: ErrorMessageProps) {
   return (
-    <div className="absolute top-10 mx-auto w-full text-pretty rounded-full bg-red-300 px-2 py-1 text-center text-sm text-red-800 md:w-1/2 md:text-base ">
+    <div
+      className={`absolute top-10 mx-auto w-full text-pretty rounded-full bg-red-300 px-2 py-1 text-center text-sm text-red-800 md:w-1/2 md:text-base ${extraStyles}`}
+    >
       {children}
     </div>
   );
