785df3802f1e6c33aeb2c996e69030aaaadd93f1
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-20 16:53:09 -0600

    Add style to menu

--- src/client/components/MyProfile/ProfileMenu.tsx
+++ src/client/components/MyProfile/ProfileMenu.tsx
@@ -15,9 +15,9 @@ export default function ProfileMenu() {
   return (
     <main className="flex h-full w-full flex-col gap-4 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-10">
       <Panel
-        bgColor="bg-lime-300"
-        borderColor="border-lime-600"
-        extraStyle="md:col-start-1 md:row-start-1 md:absolute md:bottom-0 right-0"
+        bgColor="bg-lime-300 hover:bg-lime-200 transition-all duration-500"
+        borderColor="border-lime-600 hover:border-lime-700"
+        extraStyle="md:col-start-1 md:row-start-1 md:absolute md:bottom-0 right-0 hover:brightness-105 hover:drop-shadow-xl hover:shadow-lime-500"
       >
         <Link
           to="/my-profile/library"
@@ -28,9 +28,9 @@ export default function ProfileMenu() {
         </Link>
       </Panel>
       <Panel
-        bgColor="bg-sky-300"
-        borderColor="border-sky-600"
-        extraStyle="md:col-start-2 md:row-start-1 md:absolute md:bottom-0 md:left-0"
+        bgColor="bg-sky-300 hover:bg-sky-200 transition-all duration-500"
+        borderColor="border-sky-600 hover:border-sky-700"
+        extraStyle="md:col-start-2 md:row-start-1 md:absolute md:bottom-0 md:left-0 hover:brightness-105 hover:drop-shadow-xl hover:shadow-sky-500"
       >
         <Link
           to="/my-profile/exercises"
@@ -41,9 +41,9 @@ export default function ProfileMenu() {
         </Link>
       </Panel>
       <Panel
-        bgColor="bg-amber-300"
-        borderColor="border-amber-600"
-        extraStyle="md:col-start-1 md:row-start-2 md:absolute md:top-0 md:right-0"
+        bgColor="bg-amber-300 hover:bg-amber-200 transition-all duration-500"
+        borderColor="border-amber-600 hover:border-amber-700"
+        extraStyle="md:col-start-1 md:row-start-2 md:absolute md:top-0 md:right-0 hover:brightness-105 hover:drop-shadow-xl hover:shadow-amber-500"
       >
         <Link to="/my-profile/urls" className="relative h-full w-full md:block">
           <h2 className={`${headerStyle} text-amber-800`}>URLs</h2>
@@ -51,9 +51,9 @@ export default function ProfileMenu() {
         </Link>
       </Panel>
       <Panel
-        bgColor="bg-red-300"
-        borderColor="border-red-600"
-        extraStyle="md:col-start-2 md:row-start-2 md:absolute md:top-0 md:left-0"
+        bgColor="bg-red-300 hover:bg-red-200 transition-all duration-500"
+        borderColor="border-red-600 hover:border-red-700"
+        extraStyle="md:col-start-2 md:row-start-2 md:absolute md:top-0 md:left-0 hover:brightness-105 hover:drop-shadow-xl hover:shadow-red-500"
       >
         <Link
           to="/my-profile/issues"
