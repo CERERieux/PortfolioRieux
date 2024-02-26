dc4a9e01787bb5dde0ed94fe778d1c08e2354d84
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-26 16:02:43 -0600

    Add props to fit in Dialog component

--- src/client/components/SystemDesign/Button.tsx
+++ src/client/components/SystemDesign/Button.tsx
@@ -5,21 +5,33 @@ interface PropsButton {
   children: React.ReactNode;
   onClick?: () => void;
   disabled?: boolean;
+  formmethod?: string;
+  name?: string;
+  id?: string;
+  type?: "button" | "submit" | "reset" | undefined;
 }
 export default function Button({
+  id,
+  name,
   color,
   xSize,
   mediaSize,
   children,
   onClick,
   disabled,
+  formmethod,
+  type,
 }: PropsButton) {
   const size = xSize ?? "w-1/2";
   return (
     <button
       className={`rounded-xl border px-2 py-1 hover:border-black hover:text-white ${color} ${size} ${mediaSize}`}
+      id={id}
+      name={name}
       onClick={onClick}
       disabled={disabled}
+      formMethod={formmethod}
+      type={type}
     >
       {children}
     </button>
