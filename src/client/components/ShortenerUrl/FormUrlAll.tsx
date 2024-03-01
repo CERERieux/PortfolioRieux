ba6561d30f41eb600921faedf488a4dd60a65c7e
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-02-29 19:45:37 -0600

    Almost finish component, miss description

---
+++ src/client/components/ShortenerUrl/FormUrlAll.tsx
@@ -0,0 +1,93 @@
+import { type ChangeEvent, type FormEvent, useState } from "react";
+import Button from "../SystemDesign/Button";
+import Form from "../SystemDesign/Form";
+import Input from "../SystemDesign/Input";
+import LabelForm from "../SystemDesign/LabelForm";
+import TitleForm from "../SystemDesign/TitleForm";
+import TitleInput from "../SystemDesign/TitleInput";
+import type { ShortUrlResult } from "../../types";
+import axios from "axios";
+
+interface FormUrlAllProps {
+  token: string;
+  setError: React.Dispatch<React.SetStateAction<string>>;
+  setShortUrl: React.Dispatch<React.SetStateAction<string>>;
+  setOriginalUrl: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function FormUrlAll({
+  setError,
+  setShortUrl,
+  setOriginalUrl,
+  token,
+}: FormUrlAllProps) {
+  const [userUrl, setUserUrl] = useState(""); // STate to handle user link
+  // Auxliar function to save user input into state
+  const handleUserInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserUrl(e.target.value);
+  };
+  // Auxiliar function to save the link in the database if successful
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Sent the request to the endpoint with the user link
+    const fetchResult = await axios<ShortUrlResult>({
+      url: "/cYSvQmg9kR/basic/shorturl",
+      method: "post",
+      data: {
+        url: userUrl,
+      },
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then(({ data }) => {
+        // Get the data if successful
+        return data;
+      })
+      .catch(err => {
+        // If an error happened, return it
+        console.error(err);
+        return { error: err.response.data.error.error as string };
+      });
+    // If error exist after saving, show it
+    if ("error" in fetchResult) {
+      setError(fetchResult.error);
+      setShortUrl("");
+      setOriginalUrl("");
+    } else {
+      // Else, show the result of saving the user link into database
+      setUserUrl("");
+      setError("");
+      setShortUrl(fetchResult.short_url);
+      setOriginalUrl(fetchResult.original_url);
+    }
+    return null;
+  };
+
+  // Return the form that handle user input
+  return (
+    <Form submitFn={handleSubmit} mdMedia="">
+      <TitleForm firstColor="first-letter:text-amber-500 first-letter:text-2xl">
+        Add a new link to the table.
+      </TitleForm>
+      <LabelForm>
+        <TitleInput firstColor="first-letter:text-amber-500 first-letter:text-xl w-32">
+          Link{" "}
+        </TitleInput>
+        <Input
+          name="ProfileCreateUserLinkInput"
+          type="text"
+          value={userUrl}
+          onChange={handleUserInput}
+          lineStyle
+          size={30}
+          canBeTooLong
+        />
+      </LabelForm>
+      <Button
+        color="bg-blue-300 border-blue-500 hover:bg-lime-700 hover:border-lime-300 transition-all"
+        xSize="w-32"
+      >
+        Create!
+      </Button>
+    </Form>
+  );
+}
--- src/client/components/ShortenerUrl/ShortenerUrl.tsx
+++ src/client/components/ShortenerUrl/ShortenerUrl.tsx
@@ -1,65 +1,61 @@
-import { useState, type FormEvent, type ChangeEvent } from "react"
-import type { ShortUrlResult } from "../../types"
-import { useUser } from "../../store/user"
+import { useState } from "react";
+import { useUser } from "../../store/user";
+import FormUrlAll from "./FormUrlAll";
+import CustomBackground from "../SystemDesign/CustomBackground";
 
 export default function ShortenerUrl() {
-    const [userUrl, setUserUrl] = useState("")
-    const [shortUrl, setShortUrl] = useState("")
-    const [error, setError] = useState("")
-    const { token } = useUser()
+  const [shortUrl, setShortUrl] = useState(""); // State to save the short url if adding was successful
+  const [originalUrl, setOriginalUrl] = useState(""); // State to save the original url if adding was successful
+  const [error, setError] = useState(""); // State to show an error if there is one
+  const { token } = useUser(); // Token if user is logged in to save it into their user
+  // Auxiliar to give style to almost all the boxes
+  const bgStyle =
+    "rounded-lg border border-x-2 border-y-2 border-slate-50 bg-black/70";
 
-    const handleUserInput = (e: ChangeEvent<HTMLInputElement>) => {
-        setUserUrl(e.target.value)
-    }
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const fetchResult = await fetch("/cYSvQmg9kR/basic/shorturl", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                url: userUrl
-            })
-        }).then(async (data) => {
-            if (data.ok) {
-                const shortUrl: ShortUrlResult = await data.json()
-                return shortUrl
-            }
-            return { error: "Can't make your short URL right now" }
-        })
-        if ("error" in fetchResult) {
-            setError(fetchResult.error)
-            setShortUrl("")
-        }
-        else {
-            setUserUrl("")
-            setShortUrl(fetchResult.short_url)
-            setError("")
-        }
-        return null
-    }
-
-    return (
-        <div>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="">
-                        URL you want to shortener:
-                        <input type="text" value={userUrl} onChange={handleUserInput} />
-                    </label>
-                </form>
-            </div>
-            {shortUrl !== "" &&
-                <div>
-                    <p>Your short URL is: {shortUrl}</p>
-                </div>}
-            {error !== "" &&
-                <div>
-                    <p>{error}</p>
-                </div>}
-        </div>
-    )
+  // Return the component to shortener Links
+  return (
+    <CustomBackground
+      bgImg="before:bg-[url('/bgLinks.jpg')] before:opacity-90"
+      styles="flex h-full w-full flex-col items-center justify-start gap-8 px-4 py-2 font-digitalDisplay overflow-y-auto text-slate-50"
+    >
+      <header>
+        <h1 className="text-3xl first-letter:text-4xl first-letter:text-yellow-200">
+          Shortener URL
+        </h1>
+        <p>{`Here you can get a short link through my "service".`}</p>
+      </header>
+      <section className={`h-1/4 px-8 py-4 ${bgStyle}`}>
+        <FormUrlAll
+          setError={setError}
+          setShortUrl={setShortUrl}
+          setOriginalUrl={setOriginalUrl}
+          token={token}
+        />
+      </section>
+      {shortUrl !== "" && (
+        <article className={`w-full md:w-1/2 ${bgStyle} break-words px-4 py-2`}>
+          <h2 className="text-xl first-letter:text-2xl first-letter:text-yellow-200">
+            Short URL:
+            <span className="pl-4 text-lg text-lime-400">
+              <b>{shortUrl}</b>
+            </span>
+          </h2>
+          <p className="text-sm first-letter:text-lg first-letter:text-yellow-200">
+            Original Url:
+            <span className="pl-4 text-lime-400">{originalUrl}</span>
+          </p>
+        </article>
+      )}
+      {error !== "" && (
+        <article className="rounded-lg border border-x-2 border-y-2 border-slate-50 bg-red-500/80 px-8 py-4">
+          <h2 className="text-xl first-letter:text-2xl first-letter:text-red-100">
+            Error:
+          </h2>
+          <p>
+            <b>{error}</b>
+          </p>
+        </article>
+      )}
+    </CustomBackground>
+  );
 }
