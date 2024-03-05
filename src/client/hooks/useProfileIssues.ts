297771891f039760077484f2eaba6dfaca17c5ed
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-03-04 18:59:03 -0600

    Remove extra logic

--- src/client/hooks/useProfileIssues.ts
+++ src/client/hooks/useProfileIssues.ts
@@ -2,23 +2,18 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useVerification } from "./useVerification";
 import { useUser } from "../store/user";
 import { updateIssue, getIssues } from "../services/issues";
-import { useEffect, useState } from "react";
+import type { ReqBodyIssue } from "../../server/types/advanced";
 
 export function useProfileIssues() {
   const client = useQueryClient();
   const { token, validFetch, errorAuth } = useVerification();
   const { username } = useUser();
-  const [enable, setEnable] = useState(false);
 
   const getIssuesProfile = useQuery({
     queryKey: ["Issues", username],
     queryFn: () => getIssues({ created_by: username, token }),
-    enabled: validFetch || enable,
+    enabled: validFetch,
   });
-
-  useEffect(() => {
-    if (getIssuesProfile.isSuccess) setEnable(false);
-  }, [getIssuesProfile.isFetching]);
 
   const updateIssueProfile = useMutation({
     mutationFn: updateIssue,
@@ -31,8 +26,9 @@ export function useProfileIssues() {
     data: getIssuesProfile.data,
     error: getIssuesProfile.error,
     errorAuth,
-    getNewSearch: setEnable,
+    updateOneIssue: ({ _id, project, text, title }: ReqBodyIssue) => {
+      updateIssueProfile.mutate({ token, _id, project, text, title });
+    },
     updateIssue: updateIssueProfile,
-    token,
   };
 }
