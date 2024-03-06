d04a97529e6ee6bbb08a43f79b378cda14f7dc43
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-03-06 14:03:30 -0600

    New component for DS

---
+++ src/client/components/SystemDesign/CloseNavButton.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface CloseNavButtonProps {
+  handleOpacity: () => void;
+  position?: string;
+}
+export function useCloseNavButton() {
+  const [opacity, setOpacity] = useState("opacity-0 -z-10");
+
+  const handleOpacity = () => {
+    if (opacity.includes("opacity-0")) {
+      setOpacity("opacity-100 z-10");
+    } else setOpacity("opacity-0 -z-10");
+  };
+  return { opacity, handleOpacity };
+}
+
+export function CloseNavButton({
+  handleOpacity,
+  position = "left-4 top-4",
+}: CloseNavButtonProps) {
+  return (
+    <button
+      className={`hover absolute ${position} rounded-full bg-slate-50 px-3 py-1 text-black transition-all hover:scale-110 hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/40 active:scale-90 active:bg-slate-900 active:shadow-none`}
+      onClick={handleOpacity}
+    >
+      X
+    </button>
+  );
+}
