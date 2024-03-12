bbf4e8861bb865552b0cb1d28590940c6557040d
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-03-11 19:51:59 -0600

    Add properties for flexibility

--- src/client/components/SystemDesign/CloseNavButton.tsx
+++ src/client/components/SystemDesign/CloseNavButton.tsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 interface CloseNavButtonProps {
   handleOpacity: () => void;
   position?: string;
+  bgColor?: string;
 }
 export function useCloseNavButton() {
   const [opacity, setOpacity] = useState("opacity-0 -z-10");
 
   const handleOpacity = () => {
     if (opacity.includes("opacity-0")) {
-      setOpacity("opacity-100 z-10");
+      setOpacity("opacity-100 z-20");
     } else setOpacity("opacity-0 -z-10");
   };
   return { opacity, handleOpacity };
@@ -18,10 +19,11 @@ export function useCloseNavButton() {
 export function CloseNavButton({
   handleOpacity,
   position = "left-4 top-4",
+  bgColor = "bg-slate-50",
 }: CloseNavButtonProps) {
   return (
     <button
-      className={`hover absolute ${position} rounded-full bg-slate-50 px-3 py-1 text-black transition-all hover:scale-110 hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/40 active:scale-90 active:bg-slate-900 active:shadow-none`}
+      className={`hover absolute ${position} rounded-full ${bgColor} px-3 py-1 text-black transition-all hover:scale-110 hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/40 active:scale-90 active:bg-slate-900 active:shadow-none`}
       onClick={handleOpacity}
     >
       X
