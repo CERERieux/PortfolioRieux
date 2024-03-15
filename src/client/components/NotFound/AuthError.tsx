6e928f90fb1af189c2d638a99a60abce8a37ad97
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-03-15 14:44:54 -0600

    Add bgs to components for 404 and errors

--- src/client/components/NotFound/AuthError.tsx
+++ src/client/components/NotFound/AuthError.tsx
@@ -1,28 +1,93 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import type { ErrorAuth } from "../../types"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import type { CauseError, ErrorAuth } from "../../types";
+import FooterAttribution from "../SystemDesign/FooterAttribution";
+import CustomBackground from "../SystemDesign/CustomBackground";
 
 interface Props {
-    errorAuth: ErrorAuth
+  errorAuth: ErrorAuth;
 }
 
+const CAUSE_ERROR_DATA = {
+  ExpiredToken: {
+    error: "Your session expired!",
+    bg: "before:bg-[url('/ExpiredBG.webp')]",
+    footer: (
+      <FooterAttribution
+        placeRef="on Freepik"
+        urlRef="https://www.freepik.com/free-photo/modern-city-night_973429.htm#fromView=search&page=1&position=0&uuid=ee5b20e1-62e6-447f-aed5-9831b2b2f1f3"
+        whatIs="Image"
+        extra=" by onlyyouqj"
+      />
+    ),
+  },
+  BadToken: {
+    error: "Can&apos;t confirm your session right now...",
+    bg: "before:bg-[url('/BGNumbers.webp')]",
+    footer: (
+      <FooterAttribution
+        placeRef="kjpargeter"
+        urlRef="https://www.freepik.com/free-photo/abstract-binary-code-background_6038281.htm#query=matrix%20background&position=2&from_view=keyword&track=ais&uuid=661ae88a-f2fc-4f4f-b659-3aff7cb0edb6"
+        whatIs="Image by"
+        extra=" on Freepik"
+      />
+    ),
+  },
+  NotAdmin: {
+    error: "You are not an admin.",
+    bg: "before:bg-[url('/BGNumbers.webp')]",
+    footer: (
+      <FooterAttribution
+        placeRef="kjpargeter"
+        urlRef="https://www.freepik.com/free-photo/abstract-binary-code-background_6038281.htm#query=matrix%20background&position=2&from_view=keyword&track=ais&uuid=661ae88a-f2fc-4f4f-b659-3aff7cb0edb6"
+        whatIs="Image by"
+        extra=" on Freepik"
+      />
+    ),
+  },
+  NotLoggedIn: {
+    error: "You are not logged in!",
+    bg: "before:bg-[url('/ExpiredBG.webp')]",
+    footer: (
+      <FooterAttribution
+        placeRef="on Freepik"
+        urlRef="https://www.freepik.com/free-photo/modern-city-night_973429.htm#fromView=search&page=1&position=0&uuid=ee5b20e1-62e6-447f-aed5-9831b2b2f1f3"
+        whatIs="Image"
+        extra=" by onlyyouqj"
+      />
+    ),
+  },
+};
+
 export default function UnauthorizedAccess({ errorAuth }: Props) {
-    const { message, cause } = errorAuth
-    const navigate = useNavigate()
-    useEffect(() => {
-        setTimeout(() => { navigate("/home") }, 3000)
-    }, [])
-    return (
-        <div>
-            {/** Depending on what cause we get, show an img or another
-             * like, if badtoken, show something about it, expired one a clock
-             * and like that
-             */}
-            {cause === "BadToken" && <p>{cause}</p>}
-            {cause === "ExpiredToken" && <p>{cause}</p>}
-            {cause === "NotAdmin" && <p>{cause}</p>}
-            {cause === "NotLoggedIn" && <p>{cause}</p>}
-            <h2>{message}</h2>
-        </div>
-    )
+  // For avatars
+  // <a href="https://www.freepik.com/free-photo/oak-forest-autumn_1239302.htm#fromView=search&page=1&position=12&uuid=29ad8a95-e5dd-43e8-ab57-a0870e7b0cb3">Image by bearfotos on Freepik</a>
+  // https://www.freepik.com/search?ai=excluded&format=search&last_filter=query&last_value=nature&orientation=square&query=nature&type=photo
+  const { message, cause } = errorAuth;
+  const data = CAUSE_ERROR_DATA[cause as CauseError];
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/home");
+    }, 5000);
+  }, []);
+  return (
+    <CustomBackground
+      bgImg={`${data.bg} before:opacity-100`}
+      styles="flex justify-center items-center w-full h-full"
+    >
+      <section className="h flex h-1/3 w-full flex-col items-center justify-center gap-6 rounded-xl bg-black/70 px-4 py-2 text-slate-200 md:w-1/2">
+        <h2 className="text-xl">
+          <span className="text-2xl text-red-400">Error:</span> {data.error}
+        </h2>
+        <h3 className="text-balance text-center italic text-yellow-200">
+          {message}
+        </h3>
+        <h4 className="text-sm italic text-lime-200">
+          You are being redirected to Home...
+        </h4>
+      </section>
+      {data.footer}
+    </CustomBackground>
+  );
 }
--- src/client/components/NotFound/NotFound.tsx
+++ src/client/components/NotFound/NotFound.tsx
@@ -1,19 +1,35 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import CustomBackground from "../SystemDesign/CustomBackground";
+import FooterAttribution from "../SystemDesign/FooterAttribution";
 
 export default function NotFound() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        setTimeout(() => { navigate("/home") }, 2000)
-    }, [])
-    return (
-        <div>
-            {/** Insert later here an image and maybe show status to home or something
-             * If not just the image, tal vez dale de apariencia a la pagina como si
-             * fuera un callejon de atras de un edificio o algo asi
-             */}
-            <h2>Error 404: Page not found</h2>
-            <h3>You are being redirected to Home...</h3>
-        </div>
-    )
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/home");
+    }, 2000);
+  }, []);
+  return (
+    <CustomBackground
+      bgImg="before:bg-[url('/NotFoundBG.webp')] before:opacity-90"
+      styles="flex justify-center items-center w-full h-full"
+    >
+      <section className="h flex h-1/3 w-full flex-col items-center justify-center gap-6 rounded-xl bg-black/70 text-slate-200 md:w-1/2">
+        <h2 className="flex flex-col items-center text-2xl">
+          <span className="text-3xl text-red-400">Error 404</span> Page not
+          found
+        </h2>
+        <h3 className="text-sm italic text-yellow-200">
+          You are being redirected to Home...
+        </h3>
+      </section>
+      <FooterAttribution
+        placeRef="on Freepik"
+        urlRef="https://www.freepik.com/free-photo/rustic-brick-wall-background_4413635.htm#fromView=search&page=1&position=5&uuid=776c0c10-c649-499e-b695-a46e1dedfe22"
+        whatIs="Image"
+        extra=" by rawpixel.com"
+      />
+    </CustomBackground>
+  );
 }
