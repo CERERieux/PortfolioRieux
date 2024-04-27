23da16f218ce740a20d26a7afe4975b80bcad954
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-04-27 06:06:30 -0600

    Fix pill bg color when not dark mode

--- src/client/components/Home/Pill.tsx
+++ src/client/components/Home/Pill.tsx
@@ -7,7 +7,9 @@ interface PillProps {
 export default function Pill({ children, to, extraStyles }: PillProps) {
   return (
     <a
-      className={`flex items-center justify-center gap-2 rounded-full border border-slate-200 px-6 py-1 shadow-md shadow-white/50 transition-all hover:scale-110 hover:bg-white/20 active:scale-95 active:bg-white/80 active:text-black ${extraStyles}`}
+      className={`flex items-center justify-center gap-2 rounded-full border border-slate-200 px-6 py-1 shadow-md shadow-white/50 transition-all hover:scale-110 active:scale-95 active:bg-white/80 active:text-black ${
+        extraStyles ?? "hover:bg-white/20"
+      }`}
       href={to}
       target="_blank"
       rel="noreferrer noopener"
