9ddc0c42e033ba15252fe73aa62f88e395ecddaa
Erik Rieux <59849778+CERERieux@users.noreply.github.com>
2025-06-23 14:41:47 -0600

    Add Laptop icon

---
+++ src/client/components/Icons/Laptop.tsx
@@ -0,0 +1,22 @@
+import { type Styles } from "../../types";
+
+export default function LaptopIcon({ styles = "", size = "24" }: Styles) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={`${size}`}
+      height={`${size}`}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-device-laptop ${styles}`}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 19l18 0" />
+      <path d="M5 6m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
+    </svg>
+  );
+}
