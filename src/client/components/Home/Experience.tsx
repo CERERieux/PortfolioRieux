383d1cbf0eb7cbf96cd782fb0c57c47e88dbb5cc
Erik Rieux <59849778+CERERieux@users.noreply.github.com>
2025-06-23 14:40:13 -0600

    Update visuals for experience

--- src/client/components/Home/Experience.tsx
+++ src/client/components/Home/Experience.tsx
@@ -1,4 +1,5 @@
 import { useSettingStore } from "../../store/settingPortfolio";
+import LaptopIcon from "../Icons/Laptop";
 import ExperienceItem from "./ExperienceItem";
 
 const EXPERIENCE_SPANISH = [
@@ -22,15 +23,25 @@ const EXPERIENCE_ENGLISH = [
 
 export default function Experience() {
   const { i18n } = useSettingStore();
+  const textHeader =
+    i18n === "English" ? "Work Experience" : "Experiencia Laboral";
   const EXPERIENCE =
     i18n === "English" ? EXPERIENCE_ENGLISH : EXPERIENCE_SPANISH;
   return (
-    <ol className="experienceObserve object-to-observe relative mt-16">
-      {EXPERIENCE.map(experience => (
-        <li className="">
-          <ExperienceItem {...experience} />
-        </li>
-      ))}
-    </ol>
+    <article
+      className="object-to-observe experienceObserve mt-4 flex w-full flex-col gap-4 px-6 py-3 transition-all duration-500  lg:mt-8 lg:w-3/5 lg:self-center"
+      id="AboutMe"
+    >
+      <h3 className="flex w-full items-center justify-center gap-2 text-xl text-lime-200 lg:justify-start">
+        <LaptopIcon size="26" /> {textHeader}
+      </h3>
+      <ol className=" relative mt-16">
+        {EXPERIENCE.map(experience => (
+          <li className="">
+            <ExperienceItem {...experience} />
+          </li>
+        ))}
+      </ol>
+    </article>
   );
 }
